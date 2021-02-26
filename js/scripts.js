03be8cd0dfc3f5b95a40ee600884342191803c32
Garrett Brown <garrettpaulbrown@gmail.com>
2021-02-26 13:06:45 -0800

    Add HTML and JS options.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 //Business Logic
 
-function Pizza(size) {
-    this.pizza = pizza;
+function Pizza(size, meat) {
+    this.size = size;
+    this.meat = meat;
 }
 
 
@@ -11,5 +12,10 @@ function Pizza(size) {
 $(document).ready(function() {
     $("form#order").submit(function(event) {
         event.preventDefault();
+        let name = $("input#name").val();
+        let size = $("input#size").val();
+        let meat = $("input#meat").val();
+        
+        console.log(typeof name, typeof size, typeof meat);
     })
 });
