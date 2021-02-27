57dbe5d67a1e55d8cb85aa25b781959d390b6851
Garrett Brown <garrettpaulbrown@gmail.com>
2021-02-26 18:24:24 -0800

    Test pizza.order, passed.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,22 @@
 //Business Logic
-
-function Pizza(size, meat) {
+function Pizza(size, meat, sizeCost, meatCost) {
     this.size = size;
     this.meat = meat;
+    this.sizeCost = sizeCost;
+    this.meatCost = meatCost;
 }
 
-Pizza.prototype.price = function() {
+Pizza.prototype.order = function() {
+    return this.sizeCost + this.meatCost;
+};
 
-}
+// Pizza.prototype.list = function() {
+//     return this.size + " " + this.meat;
+// };
+
+
 
 //User Logic
-
 
 $(document).ready(function() {
     $("form#order").submit(function(event) {
@@ -19,11 +25,13 @@ $(document).ready(function() {
         let sizeInput = $("#size").val().split("");
         let meatInput = $("#meat").val().split("");
         
-
         const size = sizeInput.slice(0, -1).join('');
         const sizeCost = parseInt(sizeInput.slice(-1));
         const meat = meatInput.slice(0, -1).join('');
         const meatCost = parseInt(meatInput.slice(-1));
-        console.log(size, sizeCost, meat, meatCost);
+
+        let newPizza = new Pizza(size, meat, sizeCost, meatCost);
+        let final = newPizza.order();
+        console.log(final);
     })
 });
